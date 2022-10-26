62e2cbcab71f79ca5e7d87f0d451e750028bcf52
Dabia <70469187+DabiaTheNord@users.noreply.github.com>
2022-10-26 15:36:14 -0400

    Create init.sh

---
+++ install/init.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# WARNING
+# This is just the basic UNPACKER for SpotX-GUI.
+# This unpacks essential items for the installer to run properly.
+# If you're looking for the source to the actual installer,
+# view the GitHub;
+# https://github.com/DabiaTheNord/SpotX-GUI-Linux
+
+
+
+#COPYRIGHT
+echo " ***************** "
+echo "  INITIALIZING...  "
+echo " ***************** "
+echo ""
+#INIT DIRECTORY
+echo "Making directories..."
+mkdir -p ~/.temp/
+echo "Created ~/.temp/"
+mkdir -p ~/.temp/icons/
+echo "Created ~/.temp/icons/"
+echo ""
+echo "Downloading content..."
+echo "THIS IS A PLACEHOLDER"
+exit
